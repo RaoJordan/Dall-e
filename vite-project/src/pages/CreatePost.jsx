0c928fa7f4e1d61d95bdcd268555f24c62c70ec7
Yash_Yadav <raoyash2020@gmail.com>
2023-09-09 07:00:05 +0530

    Fixed Bugs

--- vite-project/src/pages/CreatePost.jsx
+++ vite-project/src/pages/CreatePost.jsx
@@ -23,7 +23,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch('http://localhost:8080/api/v1/post', {
+        const response = await fetch('https://dall-e-63fp.onrender.com/api/v1/post', {
           method: 'POST',
           headers : {
             'Content-Type' : 'application/json',
@@ -60,7 +60,7 @@ const CreatePost = () => {
     {
       try {
         setgeneratingImg(true);
-        const response = await fetch('http://localhost:8080/api/v1/dalle', {
+        const response = await fetch('https://dall-e-63fp.onrender.com/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
